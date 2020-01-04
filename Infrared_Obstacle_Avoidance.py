2c000e0c911eb44e71c289d69efa5ff453bcc814
AshWReaper <ashton603@gmail.com>
2020-01-04 02:26:09 +0200

    Bug Fixes

--- Infrared_Obstacle_Avoidance.py
+++ Infrared_Obstacle_Avoidance.py
@@ -1,9 +1,9 @@
 import RPi.GPIO as GPIO
 import time
-from DCMotors_VS.py import DCMotors
+from DCMotors_VS import DCMotors
 
 speed_forward = 15
-speed_turn = 15
+speed_turn = 30
 
 DR = 16
 DL = 19
@@ -17,19 +17,19 @@ try:
         dcm=DCMotors()
         dcm.setPWMAY(speed_forward)
         dcm.setPWMBY(speed_forward)
-        dcm.setPWMAY(speed_turn)
         dcm.setPWMAX(speed_turn)
+        dcm.setPWMBX(speed_turn)
         
         while True:
 		DR_status = GPIO.input(DR)
 		DL_status = GPIO.input(DL)
 #		print(DR_status,DL_status)
 		if((DL_status == 0) or (DR_status == 0)):
-			dcm.left()
-			#dcm.right()
-			time.sleep(0.002)
-			dcm.stop()
-		#	print("object")
+                        dcm.stop()
+                        dcm.backward()
+                        time.sleep(0.1)
+                        dcm.left()
+                        time.sleep(0.4)
 		else:
 			dcm.forward()
 		#	print("forward")
