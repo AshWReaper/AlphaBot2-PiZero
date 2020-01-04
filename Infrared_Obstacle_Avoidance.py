49458cda2919b12e049a8b0ff0f768db5907d7be
AshWReaper <ashton603@gmail.com>
2020-01-05 00:25:57 +0200

    Comments added to file

--- Infrared_Obstacle_Avoidance.py
+++ Infrared_Obstacle_Avoidance.py
@@ -2,11 +2,11 @@ import RPi.GPIO as GPIO
 import time
 from DCMotors_VS import DCMotors
 
-speed_forward = 15
-speed_turn = 30
+speed_forward = 15 # set the forward/backward speed here
+speed_turn = 30 # set the turning speed here
 
-DR = 16
-DL = 19
+DR = 16 # set the GPIO pin number for the right hand side sensor (Dont Change This Value!!!)
+DL = 19 # set the GPIO pin number for the right hand side sensor (Dont Change This Value!!!)
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -14,25 +14,26 @@ GPIO.setup(DR,GPIO.IN,GPIO.PUD_UP)
 GPIO.setup(DL,GPIO.IN,GPIO.PUD_UP)
 
 try:
-        dcm=DCMotors()
-        dcm.setPWMAY(speed_forward)
-        dcm.setPWMBY(speed_forward)
-        dcm.setPWMAX(speed_turn)
-        dcm.setPWMBX(speed_turn)
+        dcm=DCMotors() # create instance of our DC motors class 
+        dcm.setPWMAY(speed_forward) # set the foward speed for DC Motor "A"
+        dcm.setPWMBY(speed_forward) # set the foward speed for DC Motor "B"
+        dcm.setPWMAX(speed_turn) # set the turning speed for DC Motor "A"
+        dcm.setPWMBX(speed_turn)  # set the turning speed for DC Motor "B"
         
         while True:
-		DR_status = GPIO.input(DR)
-		DL_status = GPIO.input(DL)
-#		print(DR_status,DL_status)
+		DR_status = GPIO.input(DR) # get the input from the right hand side sensor
+		DL_status = GPIO.input(DL) # get the input from the left hand side sensor
+
+                ## if either sensor picks up an obstacle....
 		if((DL_status == 0) or (DR_status == 0)):
-                        dcm.stop()
-                        dcm.backward()
+                        dcm.stop() # stop
+                        dcm.backward() # go back a bit
                         time.sleep(0.1)
-                        dcm.left()
+                        dcm.left() # turn left
                         time.sleep(0.4)
+                ## else if the terrain is clear....
 		else:
-			dcm.forward()
-		#	print("forward")
+			dcm.forward() # move forward
 
 except KeyboardInterrupt:
 	GPIO.cleanup();
