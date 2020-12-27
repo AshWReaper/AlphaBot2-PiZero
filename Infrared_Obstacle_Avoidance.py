19951e80ce32ff3b1993061ad1d05006c7075eef
AshWReaper <ashton603@gmail.com>
2020-12-28 01:11:10 +0200

    Adding an "Autonomous" program for more fun...

--- Infrared_Obstacle_Avoidance.py
+++ Infrared_Obstacle_Avoidance.py
@@ -8,6 +8,9 @@ speed_turn = 30 # set the turning speed here
 DR = 16 # set the GPIO pin number for the right hand side sensor (Dont Change This Value!!!)
 DL = 19 # set the GPIO pin number for the right hand side sensor (Dont Change This Value!!!)
 
+FWD_STATUS = False; # set the initial forward status (false is recommended)
+TEMP_STORAGE = 0; # used to count how many loop itterations occur when moving forward
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(DR,GPIO.IN,GPIO.PUD_UP)
@@ -26,6 +29,8 @@ try:
                 
                 ## if the oblsicle is detected by the right-hand side sensor...
                 if((DR_status == 0) and (DL_status == 1)):
+                        TEMP_STORAGE = 0;
+                        FWD_STATUS = False # change forward status to false
                         dcm.stop() # stop
                         dcm.left() # turn left
                         time.sleep(0.4)
@@ -33,6 +38,8 @@ try:
                         
                 ## if the oblsicle is detected by the left-hand side sensor...
                 if((DL_status == 0) and (DR_status == 1)):
+                        TEMP_STORAGE = 0;
+                        FWD_STATUS = False # change forward status to false
                         dcm.stop() # stop
                         dcm.right() # turn right
                         time.sleep(0.4)
@@ -40,6 +47,8 @@ try:
                         
                 ## if the obsicle is deteted by both the left and right-hand side sensors...
                 if((DR_status == 0) and (DL_status == 0)):
+                        TEMP_STORAGE = 0;
+                        FWD_STATUS = False # change forward status to false
                         dcm.stop() # stop
                         dcm.backward() # go backwards 
                         time.sleep(3)
@@ -55,7 +64,21 @@ try:
                         ## try add some extra movements in here while the bot is moving forward, but there must be no obsticals in sight...
                         FWD_STATUS = dcm.forward()
                         print(FWD_STATUS)
-                        #while (FWD_STATUS == True):
+                        print(TEMP_STORAGE)
+                        if(FWD_STATUS == True):
+                                print("Moving Forward With Intent!")
+                                TEMP_STORAGE+=1
+
+                        if(FWD_STATUS == False):
+                                print("doing something else with intent")
+
+                while(TEMP_STORAGE > 3000):
+                        print("FOOOO!")
+                        print(DR_status)
+                        DR_status = GPIO.input(DR)
+                        if((DR_status == 0) or (DL_status == 0)):
+                                TEMP_STORAGE = 0
+                
 except KeyboardInterrupt:
 	GPIO.cleanup();
 
